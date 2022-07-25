229b4d76c053996fed0a490a397f0f12c6afa284
ariana-ssilva <arianamoreira5021@gmail.com>
2022-07-24 21:17:35 -0300

    update readme and some styling.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# mario-jumps
-Creating a game using html, css and javascript
+# 🎮 Mario Jumps Game 🎮
+
+Creating a game using HTML5, CSS3 and Javascript, based in this [tutorial](https://www.youtube.com/watch?v=r9buAwVBDhA).
+This project was developed just to exercise some concepts during the vacation.
+
+<center>
+
+![Mario Jumps Game](./images/mario-jumps.gif)
+
+</center>
+
--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ const loop = setInterval(() => {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
         mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`;
+        mario.style.bottom = `${marioPosition}px`; 
         mario.src = './images/game-over.png';
         mario.style.width = '75px';
         mario.style.marginLeft = '50px';
